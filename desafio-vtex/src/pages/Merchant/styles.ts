4dec54dc118d2f475393dd8a7358083e4280dd3a
Wosley Vacemberg <wosleyv@gmail.com>
2020-05-04 18:39:31 -0300

    rm master

--- desafio-vtex/src/pages/Merchant/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import Bg from '../../assets/background-login.png';
-
-export const Container = styled.div<any>`
-  display: flex;
-  background: url(${Bg}) no-repeat center;
-  background-size: cover;
-`
