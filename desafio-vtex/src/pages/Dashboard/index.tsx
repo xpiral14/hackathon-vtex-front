dd2acbe1054828bee31351e1308de2e2f98e9f23
xpiral14 <sa.reis1999@gmail.com>
2020-05-04 21:35:09 -0300

    Created land-page and updated Header component

--- desafio-vtex/src/pages/Dashboard/index.tsx
+++ desafio-vtex/src/pages/Dashboard/index.tsx
@@ -1,16 +1,133 @@
 import React from 'react';
+import market from '../../assets/market.png';
+import integration from '../../assets/integration.png';
+import estoqueFim from '../../assets/estoque-fim.png';
+import bannerBackground from '../../assets/commerce.png';
+import phoneBackground from '../../assets/phone.png';
+import notebookBackground from '../../assets/background-notebook.png';
+import pechinchaAiCicle from '../../assets/pechinchaAi-cicle.png';
+import principaisVantagens from '../../assets/principais-vantagens.png';
 
-import { Container, Banner } from './styles';
+import quemSomos from '../../assets/quem-somos.png';
+import restContent from '../../assets/rest-content.png';
+import cicle from '../../assets/cicle.png';
+import {
+  Container,
+  Banner,
+  BannerMiddle,
+  Content,
+  HowWork,
+  InfoContainer,
+  InfoTextContent,
+  InfoImageContent,
+  TitleContent,
+  ImageContent,
+} from './styles';
 
 // import backgroundBanner from '../../assets/background-banner.jpeg';
-
+interface infoInterface {
+  text: string;
+  image: string;
+  alignText: 'right' | 'left';
+}
+const infos: infoInterface[] = [
+  {
+    text:
+      'Primeiro de tudo, é necessário ter um E-Commerce. Caso você não possua esse serviço, providenciamos pra você!',
+    image: market,
+    alignText: 'right',
+  },
+  {
+    text:
+      'Com a integração realizada, o APP armazena os produtos e a quantidade em estoque, de cada produto.',
+    image: integration,
+    alignText: 'left',
+  },
+  {
+    text:
+      'Assim que o estoque do produto chega ao fim, muitos lojistas param de vendê-lo até realizarem o próximo pedido e tê-lo em estoque.&#013;Assim, é perdida a fidelização do cliente, e consequentemente o lucro das vendas!',
+    image: estoqueFim,
+    alignText: 'right',
+  },
+];
 const Dashboard: React.FC = () => {
   return (
     <Container>
-      <Banner>
-  
+      <Banner bg={bannerBackground}>
+        <BannerMiddle bgColor="#100F0F30">
+          <h1>
+            PechinchAKI, o APP perfeito pra você que deseja aumentar o fluxo de
+            vendas e a rotatividade de seu estoque.
+          </h1>
+          <span>
+            <br />
+            Seja mais!
+            <br />
+            Seja PechinchAí!
+          </span>
+        </BannerMiddle>
       </Banner>
+      <Content>
+        <HowWork id = "como-funciona">
+          <h2>Como funciona?</h2>
 
+          {infos.map((info) => (
+            <InfoContainer alignText={info.alignText}>
+              {info.alignText === 'right' ? (
+                <>
+                  <InfoImageContent>
+                    <img src={info.image} />
+                  </InfoImageContent>
+                  <InfoTextContent>
+                    <h1>{info.text}</h1>
+                  </InfoTextContent>
+                </>
+              ) : (
+                <>
+                  <InfoTextContent>
+                    <h1>{info.text}</h1>
+                  </InfoTextContent>
+                  <InfoImageContent>
+                    <img src={info.image} />
+                  </InfoImageContent>
+                </>
+              )}
+            </InfoContainer>
+          ))}
+        </HowWork>
+
+        <Banner bg={notebookBackground}>
+          <img src={cicle} style = {{marginTop: -200, width: "100%", maxWidth: "700px"}}/>
+          <TitleContent>
+            <h2>Sistema de Revenda</h2>
+          </TitleContent>
+          <br />
+          <BannerMiddle bgColor="#133a7560">
+            <h1>
+              E como o lojista ganha, revendendo o produto de outro lojista?
+              <br />
+              <br />
+              Com a revenda, o lojista que intermediou essa comercialização, por
+              falta de estoque, ganha uma comissão por cada venda.
+              <br />
+              <br />
+              Essa comissão é definida previamente, por produto vendido ou pelo
+              valor total da compra.
+            </h1>
+          </BannerMiddle>
+        </Banner>
+
+        <ImageContent>
+          <img src={pechinchaAiCicle} />
+        </ImageContent>
+        <ImageContent>
+          <img src={principaisVantagens} />
+        </ImageContent>
+        <ImageContent id = "como-somos">
+          <img src={quemSomos} />
+        </ImageContent>
+       
+      </Content>
     </Container>
   );
 };
--- desafio-vtex/src/pages/Dashboard/styles.ts
+++ desafio-vtex/src/pages/Dashboard/styles.ts
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
-import Bg from '../../assets/commerce.png';
-import Bg2 from '../../assets/phone.png'
-
+import phoneBackground from '../../assets/phone.png';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -12,13 +10,12 @@ export const Container = styled.div`
 export const Title = styled.h1`
   font-size: 30px;
   color: #fff;
-  display:flex;
+  display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 300px;
   background: rgba(16, 15, 15, 0.3);
-
 `;
 
 export const Image = styled.img`
@@ -26,8 +23,11 @@ export const Image = styled.img`
   width: 100%;
 `;
 
-export const Banner = styled.div`
-  background: url(${Bg}) no-repeat center;
+interface BannerInterface {
+  bg: string;
+}
+export const Banner = styled.main<BannerInterface>`
+  background: url(${(p) => p.bg}) no-repeat center;
   background-size: cover;
   flex: 1;
   display: flex;
@@ -35,14 +35,83 @@ export const Banner = styled.div`
   justify-content: center;
   align-items: center;
   color: #fff;
+  min-height: 100vh;
 `;
 
-export const BannerMiddle = styled.div`
-  background: url(${Bg2}) no-repeat;
-  float: left;
-  height: 500px;
+interface BannerMiddleInterface {
+  bgColor: string;
+}
+export const BannerMiddle = styled.section<BannerMiddleInterface>`
+  background-color: ${(p) => p.bgColor};
+  text-align: center;
+  padding: 30px;
 
-  `;
+  width: 100%; 
+  h1,
+  span {
+    font-weight: bold;
+    font-size: 28px;
+  }
+  span {
+  }
+`;
 
-
+export const Content = styled.section``;
 // pega ela com alta resolução no figma pera
+
+export const HowWork = styled.section`
+  padding: 30px 0 200px 0;
+  background: url(${phoneBackground}) no-repeat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h2 {
+    color: #f2e3e3;
+    font-size: 28px;
+  }
+`;
+interface InfoContainerInterface {
+  alignText: 'right' | 'left';
+}
+export const InfoContainer = styled.section<InfoContainerInterface>`
+  margin-top: 30px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: ${(p) =>
+    p.alignText === 'right' ? '.3fr .7fr' : '.7fr .3fr'};
+  grid-column-gap: 30px;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+export const InfoImageContent = styled.div`
+  margin: 0 auto;
+`;
+export const InfoTextContent = styled.section`
+  margin: 0 auto;
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #133a7560;
+  color: #e2e4f1;
+  text-align: center;
+  font-size: 28px;
+  width: 100%;
+`;
+
+export const TitleContent = styled.div`
+  padding: 10px 20px;
+  font-size: 25px;
+  font-weight: bold;
+  border-radius: 30px;
+  background: #1f212680;
+`;
+
+export const ImageContent = styled.div`
+  img {
+    width: 100%;
+  }
+`;
