f59017d06e8a85d7c8b48aed36a9451080d85f4b
xpiral14 <sa.reis1999@gmail.com>
2020-05-04 23:49:09 -0300

    Added page suppliers

--- desafio-vtex/src/pages/Logon/styles.ts
+++ desafio-vtex/src/pages/Logon/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 import BackGroundLogin from '../../assets/background-login.png';
-
+import { shade } from 'polished';
 
 export const Container = styled.div`
   width: 100vw;
@@ -24,10 +24,10 @@ export const Logo = styled.div`
   align-items: center;
   justify-content: center;
   flex: 1;
-    img {
-        height: 30vh;
-        width: 20vw;
-      }
+  img {
+    height: 30vh;
+    width: 20vw;
+  }
 `;
 
 export const Formulario = styled.form`
@@ -36,15 +36,14 @@ export const Formulario = styled.form`
   align-items: center;
   justify-content: center;
   margin-top: 30px;
-    img {
-      align-items: center;
-      justify-content: center;
-      height: 30vh;
-      width: 20vw;
-      margin-top: -30vh;
-    }
+  img {
+    align-items: center;
+    justify-content: center;
+    height: 30vh;
+    width: 20vw;
+    margin-top: -30vh;
+  }
 `;
-
 
 export const ApoiadoresText = styled.div`
   margin-top: 30px;
@@ -52,18 +51,17 @@ export const ApoiadoresText = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-    strong {
-      font-style: normal;
-      font-weight: bold;
-      font-size: 28px;
-      line-height: 33px;
-      display: flex;
-      align-items: center;
-      text-align: center;
-      color: #F9E9E9;
-    }
-`
-
+  strong {
+    font-style: normal;
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 33px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: #f9e9e9;
+  }
+`;
 
 export const ApoiadoresImg = styled.div`
   width: 100%;
@@ -71,11 +69,28 @@ export const ApoiadoresImg = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-    img {
-      justify-content: center;
-    }
-`
+  img {
+    justify-content: center;
+  }
+`;
 
+export const A = styled.a`
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  width: 150px;
+  background: #445cae;
+  margin: 0 auto;
 
-
-
+  margin-top: 60px;
+  font-weight: bold;
+  font-size: 14px;
+  color: #fff;
+  border-radius: 16px;
+  border: 0;
+  transition: background-color 0.2s;
+  padding: 10px 20px;
+  &:hover {
+    background: ${shade(0.2, '#445CAE')};
+  }
+`;
--- desafio-vtex/src/pages/Suppliers/styles.ts
+++ desafio-vtex/src/pages/Suppliers/styles.ts
@@ -2,54 +2,87 @@ import styled from 'styled-components';
 
 import BackGroundLogin from '../../assets/background-login.png';
 
-export const Container = styled.div<any>`
+export const Container = styled.div`
   height: 100vh;
-
+  /* width: 100%; */
   display: flex;
-  align-items: stretch;
-`
+  justify-content: space-between;
+`;
 
-
-
-export const Background = styled.div`
-  flex: 1;
+export const Background = styled.main`
+  width: 100%;
   background: url(${BackGroundLogin}) no-repeat center;
   background-size: cover;
-      div {
-        background: #F9F3F3;
-        width: 80px;
-        height: 50px;
-        display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
-        button {
-          align-items: center;
-          justify-content: center;
-          display: flex;
-        }
+`;
+
+export const Content = styled.section`
+  /* width: 100%; */
+`;
+
+export const SupplierHeader = styled.div`
+  display: flex;
+  padding: 20px;
+`;
+export const PerfilSupplierImage = styled.div``;
+
+export const SupplierInfo = styled.div`
+  background: #f9f2f2;
+  padding: 15px;
+  /* flex: 1; */
+  width: 100%;
+  margin-left: 20px;
+  h1 {
+    color: #575f89;
+    font-size: 28px;
+  }
+  div:first-child {
+    padding: 10px;
+    border-bottom: 1px solid #000;
+  }
+  div:last-child {
+    margin-top: 20px;
+    text-align: center;
+    color: #575f89;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+
+    span:last-child {
+      margin-top: 20px;
+      font-size: 25px;
     }
+  }
+`;
 
-` ;
+export const SolictedProductContent = styled.div`
+  padding: 20px;
+  background: #f9f2f2;
+  display: flex;
+  flex-direction: column;
+  & > div:first-child {
+    text-align: center;
+    padding: 10px;
+    border-bottom: 1px solid black;
+  }
 
-//  export const Formulario = styled.div`
-//    display: flex;
-//    align-items: center;
-//    justify-content: center;
-//    font-size: 80px;
+  & > div:last-child {
+    display: flex;
+  }
+`;
 
-//     strong {
-//       justify-content: center;
-//       align-items: center;
-//       color: #FFFF;
-//       font-size:25px;
-//     }
-    
-//      img {
-//        align-items: center;
-//        justify-content: center;
-//        height: 30vh;
-//        width: 20vw;
-//        margin-top: -30vh;
-//      }
-//  `;
+export const ProductImageContent = styled.div``;
+export const SendDateContent = styled.div``;
+
+export const ProductContent = styled.div`
+  text-align: center;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;  
+`;
+export const Button = styled.button`
+  padding: 5px 10px;
+  color: #f1ebeb;
+`;
